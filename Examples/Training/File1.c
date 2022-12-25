cfc1082b5f50331c5f95669f929be503794389f1
AzureAD\OmarAzmy <omar.azmy@partner.magna.com>
2022-12-25 16:16:22 +0200

    adding example file

--- Examples/Training/File1.c
+++ Examples/Training/File1.c
@@ -1,14 +1,16 @@
+#include <stdio.h>
+
 #define ROR(REG,num) ( REG= (REG>>num) | (REG<<(8-num)) )
-
 #define ROL(REG,num) ( REG= (REG<<num) | (REG>>(8-num)) )
-
 #define BIT_IS_SET(REG,BIT) ( REG & (1<<BIT) )
-
+#define SET_BIT(REG,BIT) ((REG) |= (1 << (BIT)))
 #define BIT_IS_CLEAR(REG,BIT) ( (REG & (1<<BIT)) == 0 )
+#define CLEAR_BIT(REG,BIT) ((REG) &= ~(1 << (BIT)))
 
 
 
-int main(){
+int main(void)
+{
 	unsigned char var=0;
 	SET_BIT(var,0);
 	SET_BIT(var,1);
@@ -16,17 +18,19 @@ int main(){
 	printf("var=0x%x\n",var);
 	CLEAR_BIT(var,2);
 	printf("var=0x%x\n",var);
-	TOGGLE_BIT(var,2);
+	//TOGGLE_BIT(var,2);
 	printf("var=0x%x\n",var);
 	ROR(var,1);
 	printf("var=0x%x\n",var);
 	ROL(var,1);
 	printf("var=0x%x\n",var);
-	if(BIT_IS_SET(var,2)){
+	if(BIT_IS_SET(var,2))
+	{
 		CLEAR_BIT(var,2);
 		printf("var=0x%x\n",var);
 	}
-	if(BIT_IS_CLEAR(var,2)){
+	if(BIT_IS_CLEAR(var,2))
+	{
 		SET_BIT(var,2);
 		printf("var=0x%x\n",var);
 	}
