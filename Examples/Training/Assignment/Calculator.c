0fc04a12ad93ef461c84ead46e85477168db8d43
Mohamed.elhenawy <mohamed.elhenawy@vehiclevo.com>
2023-01-16 18:39:43 +0200

    Adding the sine function body

--- Examples/Training/Assignment/Calculator.c
+++ Examples/Training/Assignment/Calculator.c
@@ -64,6 +64,13 @@ void factorial()
 void sine()
 {
      //enter your code here please :)
+     double x;
+    double result;
+
+    scanf("%lf",&x);
+    result = sin(x);
+    printf("sin(%.2lf) = %.2lf\n", x, result);
+
 }
 
 // Function to calculate cosine of angle in radians
