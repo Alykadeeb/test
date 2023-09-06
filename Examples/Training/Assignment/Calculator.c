c8116ffa9c51b2a63484f9c6d9b20646f87e1381
Abdelrhman Sanad <abdelrhman.sanad@vehiclevo.com>
2023-09-06 17:16:37 +0300

    add

--- Examples/Training/Assignment/Calculator.c
+++ Examples/Training/Assignment/Calculator.c
@@ -15,18 +15,12 @@ int menu()
 }
 
 // Function to add numbers
-<<<<<<< HEAD
 int addition(int x0, int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8)
 {
+    //enter your code here please :)
     int sum = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8;
 	return sum;
-=======
-int addition(int x, int y,int z)//my change
-{
-    //enter your code here please :)
-    return x+y+z;
->>>>>>> 16a5f34ef9c839470f6b403bb18e7d26978ed510
-}
+
 
 // // Function to subtract numbers
 // void subtraction()
