94f13e13e49efdf8df3104dde014e782c32c113d
Mostafa Issa <mostafa.issa@vehiclevo.com>
2023-09-06 16:28:03 +0300

    Mostafa_ISSA

--- Examples/Training/Assignment/Calculator.c
+++ Examples/Training/Assignment/Calculator.c
@@ -15,10 +15,10 @@ int menu()
 }
 
 // Function to add numbers
-int addition(int x, int y,int z)//my change
+void addition(int x,int y,int z,int a,int b)
 {
     //enter your code here please :)
-    return x+y+z;
+    
 }
 
 // // Function to subtract numbers
